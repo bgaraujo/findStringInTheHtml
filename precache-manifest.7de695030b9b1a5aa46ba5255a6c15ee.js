a51e001bb7d4290994baa2be350a547bd9feb4bc
bruno.araujo.acc <bruno.araujo@accurate.com.br>
2020-10-07 16:59:32 -0300

    Updates

---
+++ precache-manifest.7de695030b9b1a5aa46ba5255a6c15ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf314f144dd3cf459d9b9121fdf9625f",
+    "url": "/findStringInTheHtml/index.html"
+  },
+  {
+    "revision": "73040a25a937b0d300e8",
+    "url": "/findStringInTheHtml/static/css/main.0c269562.chunk.css"
+  },
+  {
+    "revision": "63efc90a2e17e458bc9f",
+    "url": "/findStringInTheHtml/static/js/2.31197ddf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findStringInTheHtml/static/js/2.31197ddf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73040a25a937b0d300e8",
+    "url": "/findStringInTheHtml/static/js/main.dfcf53b5.chunk.js"
+  },
+  {
+    "revision": "c6c3cc16f6448bc2222a",
+    "url": "/findStringInTheHtml/static/js/runtime-main.a9d029b5.js"
+  }
+]);
